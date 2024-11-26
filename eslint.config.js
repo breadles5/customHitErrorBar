cf439ea0c5b1a9c5d731e3d0c684c7e0e795a03b
breadles5 <alextran91080@gmail.com>
2024-11-25 22:13:40 -0500

    chore: improve eslint config for stricter checks

--- eslint.config.js
+++ eslint.config.js
@@ -1,10 +1,26 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-
-/** @type {import('eslint').Linter.Config[]} */
+/** @type {import('eslint').Linter.FlatConfig[]} */
 export default [
-  {languageOptions: { globals: globals.browser }},
+  {
+    ignores: ["node_modules/", "dist/", "js/socket.js"],
+  },
+  // Main configuration
+  {
+    languageOptions: {
+      ecmaVersion: 2023,
+      sourceType: "module",
+      globals: globals.browser,
+    },
+    rules: {
+      "no-unused-vars": ["warn"],
+      eqeqeq: ["error", "always"],
+      "no-console": "off",
+      semi: ["error", "always"],
+      quotes: ["error", "double"],
+    },
+  },
+  // Include the recommended rules from @eslint/js
   pluginJs.configs.recommended,
-  no-unused-vars
 ];
