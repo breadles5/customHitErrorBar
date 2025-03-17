1ec7143e71b83a09200a9179e282e37ff6ad6426
breadles5 <alextran91080@gmail.com>
2025-03-16 23:46:01 -0400

    organize: move reset functions to new file

---
+++ src/utils/reset.ts
@@ -0,0 +1,24 @@
+import { cache, ticksWorker, statisticsWorker } from "..";
+import { resetArrow } from "./arrow";
+import { clearSD } from "./elements";
+import { rerenderTicks } from "./ticks";
+
+// extends resetUI to reset everything
+export const reset = () => {
+    resetUI();
+    cache.tickPool.set();
+    cache.statistics.averageError = 0;
+    cache.statistics.standardDeviationError = 0;
+
+    // synchronizes worker caches
+    ticksWorker.postMessage({ type: "set" });
+    statisticsWorker.postMessage({ type: "set" });
+    console.log("[Main] reset");
+};// Reset UI
+export const resetUI = () => {
+    clearSD();
+    resetArrow();
+    rerenderTicks();
+    console.log("[Main] reset UI");
+};
+
