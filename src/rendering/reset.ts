bcfa83fa6bf9f57d735df75717df8ef3bc04f42a
breadles5 <alextran91080@gmail.com>
2025-03-26 02:31:02 -0400

    wrap in requestAnimationFrame()

--- src/rendering/reset.ts
+++ src/rendering/reset.ts
@@ -1,13 +1,15 @@
 import { cache } from "..";
 import { resetArrow } from "./arrow";
 import { clearSD } from "./elements";
-import { resetTicks, updateTicks } from "./ticks";
+import { resetTicks } from "./ticks";
 
 // extends resetUI to reset everything
 export const reset = () => {
-    cache.tickPool.set();
-    resetTicks;
-    clearSD();
-    resetArrow();
-    console.log("[Main] reset");
+    requestAnimationFrame(() => {
+        cache.tickPool.set();
+        resetTicks();
+        clearSD();
+        resetArrow();
+        console.log("[Main] reset");
+    });
 };
